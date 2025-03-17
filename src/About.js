8e5a9ea03a2a5f5dc94571b14161a25e1bfa49a2
Peregrine Neubert <pneubert@inflexionpoint.ai>
2025-03-17 18:48:17 -0400

    first save

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About(){
+    return(
+        <p>About: Coach Jen
+        My name is Jen Olk and I have been an enthusiastic leader of the swim community for over 35 years.  I began teaching swim lessons as a teenager before going on to school in which I earned an undergrad degree in education and a masters in both outdoor education and experiential learning. Through this process, my knowledge of the sport of swimming developed.  This, in combination with my many years of hands-on experience, have given me significant professional experience in the sport, helping me to become an expert across all levels and abilities.
+        
+        I have been an integral part of the swim community- helping people achieve their goals in backyard pools, open water settings in lakes, and high level competitions.  Most recently, I led nearly 120 swimmers as head coach of Chelsea Aquatic club.  I created training programs for swimmers who were brand new to the sport all the way through Junior National level qualifiers and NCAA Division 1 athletes.  My love for swimming carried over into the triathlon world as I led the Chelsea Triathlon club for many years.  I work with all levels of youth and adults including swimmers with special needs or medically sensitive individuals.
+        
+        Most importantly, however, I am a mother of four.  My children have now grown old enough for me to dive full-time into my love of the water and open up a storefront.  This will allow me  to continue to grow out my dream of helping the community.  I am most passionate about teaching and look forward to having a convenient location to share this with more people. 
+        
+        </p>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './About'
+import {Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+function IndexBody(){
+  return(
+  <a
           className="App-link"
-          href="https://reactjs.org"
+          href="About.js"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          About Our Coaches
+  </a>
+  );
+}
+
+
+function App() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className='menu'>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/About'>About</Link></li>
+          </ul>
+        </div>
+       <IndexBody /> 
       </header>
     </div>
   );
+
+  
 }
 
 export default App;
